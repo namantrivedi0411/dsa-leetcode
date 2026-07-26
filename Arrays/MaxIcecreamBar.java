c416337b9b5c83dd6a7c611d95143d95bca8090a
Naman Trivedi <namantrivedi0411@gmail.com>
2026-07-26 13:17:11 +0530

    Implement maxIceCream method to calculate maximum bars

---
+++ Arrays/MaxIcecreamBar.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i=0;i<costs.length;i++){
+            if(costs[i]>coins) return count;
+            count++;
+            coins-=costs[i];
+        }
+        return count;
+    }
+}
